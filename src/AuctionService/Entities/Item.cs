132ec6801f236eecb8e7695798cf93bb14e5af4d
marco-soria <marco.soriaa7@gmail.com>
2025-03-25 08:42:18 -0500

    added entities

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace AuctionService.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+
+        public string Mileage { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public Auction Auction { get; set; }
+
+        public Guid AuctionId { get; set; }
+    }
+}
