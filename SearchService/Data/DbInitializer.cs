eca224df18b5315804b48882a94bad270e65c3c0
marco-soria <marco.soriaa7@gmail.com>
2025-03-27 21:57:50 -0500

    created sync service for auctions and search updated docker file

--- SearchService/Data/DbInitializer.cs
+++ SearchService/Data/DbInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 using System.Text.Json;
 
 namespace SearchService.Data
@@ -9,11 +10,9 @@ namespace SearchService.Data
     {
         public static async Task InitDb(WebApplication app)
         {
-            await DB.InitAsync(database: "SearchDb",
-                settings: MongoClientSettings.
-                    FromConnectionString(
-                        app.Configuration
-                        .GetConnectionString("MongoDbConnection")));
+            await DB.InitAsync("SearchDb",
+                MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
             await DB.Index<Item>()
                 .Key(x => x.Make, KeyType.Text)
                 .Key(x => x.Model, KeyType.Text)
@@ -21,14 +20,16 @@ namespace SearchService.Data
                 .CreateAsync();
 
             var count = await DB.CountAsync<Item>();
-            if (count == 0)
-            {
-                Console.WriteLine("No data - will attempt to seed");
-                var itemData = await File.ReadAllTextAsync(path: "Data/auctions.json");
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var items = JsonSerializer.Deserialize<List<Item>>(json: itemData, options: options);
-                await DB.SaveAsync(entities: items);
-            }
+
+            using var scope = app.Services.CreateScope();
+
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+            var items = await httpClient.GetItemsForSearchDb();
+
+            Console.WriteLine(items.Count + " returned from auction service");
+
+            if (items.Count > 0) await DB.SaveAsync(items);
         }
     }
 }
--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,10 +1,15 @@
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
+using SearchService.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -26,3 +31,9 @@ catch (Exception e)
 }
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
