37a2491088606313af96c4af27a1269616ac39b1
marco-soria <marco.soriaa7@gmail.com>
2025-04-04 10:10:58 -0500

    added register functionality

--- src/IdentityService/SeedData.cs
+++ src/IdentityService/SeedData.cs
@@ -66,10 +66,7 @@ public class SeedData
 
             result = userMgr.AddClaimsAsync(bob, new Claim[]{
                             new Claim(JwtClaimTypes.Name, "Bob Smith"),
-                            new Claim(JwtClaimTypes.GivenName, "Bob"),
-                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                            new Claim(JwtClaimTypes.WebSite, "http://bob.example.com"),
-                            new Claim("location", "somewhere")
+                          
                         }).Result;
             if (!result.Succeeded)
             {
